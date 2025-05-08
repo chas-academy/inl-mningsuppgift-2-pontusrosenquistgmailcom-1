d05cdfb0d9ba3889c3d83d7eea2cc2da8a4c767b
pontus.rosenquist@gmail.com <pontus.rosenquist@gmail.com>
2025-05-08 10:50:53 +0200

    commit 1

--- src/script.js
+++ src/script.js
@@ -1,23 +1,42 @@
 // Definera array av namn
-let people;
+let people = ["Viktor", "Helena", "Jordan", "Teresa", 
+  "Jane", "Steven", "Råland","Helge", 
+  "Rutger", "Melita",];
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
-  
+  const htmlSearchInput = document.getElementById("searchInput")
+  const htmlButton = document.getElementById("searchBtn")
+  let htmlDisplayResult = document.getElementById("resultDisplay")
+
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
-    
+    const searchName = htmlSearchInput.value
+    console.log(searchName)
+
     // 2. Skapa en boolean för found
-    
+    let boolFound = false
     // 3. Loopa igenom people-arrayen
-    
+    for(let person of people){
     // 4. Jämför texten med varje namn i arrayen
+      if(person === searchName){
+        // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
+        htmlDisplayResult.innerHTML = "Namn hittades: " + searchName
+        htmlDisplayResult.style.color = "blue"
+        boolFound = true
+      }
+
+    }
+    if(!boolFound){
+      htmlDisplayResult.innerHTML = "Namn hittades inte"
+      htmlDisplayResult.style.color = "red"
+    }
     
-    // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
+
     
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
     
